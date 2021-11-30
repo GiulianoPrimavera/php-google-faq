b96b088a5ebf9da4620ce4a01a37fdb2ac6ab77a
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-30 15:16:51 +0100

    adds navbar to the html page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono:wght@200;300;400&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>Google Privacy e Termini</h1>
+
+        <div class="navbar_items_container">
+            <ul class="navbar_items">
+                <li>
+                    <a href="#">Domande Frequenti</a>
+                </li>
+                <li>
+                    <a href="#">Tecnologie</a>
+                </li>
+                <li>
+                    <a href="#">Termini di servizio</a>
+                </li>
+                <li>
+                    <a href="#">Norme sulla privacy</a>
+                </li>
+                <li>
+                    <a href="#">Introduzione</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</body>
+</html>
